3c4694bb414edea6e5808d47c2c40503540f54f4
Raja Kolli <rajadilipkolli@gmail.com>
2026-04-30 21:06:22 +0000

    more modules

--- boot-pulsar/src/test/java/com/example/demo/PulsarIntegrationTests.java
+++ boot-pulsar/src/test/java/com/example/demo/PulsarIntegrationTests.java
@@ -8,9 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.pulsar.reactive.core.ReactivePulsarTemplate;
-import org.testcontainers.containers.PulsarContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.pulsar.PulsarContainer;
 import org.testcontainers.utility.DockerImageName;
 import reactor.test.StepVerifier;
 
