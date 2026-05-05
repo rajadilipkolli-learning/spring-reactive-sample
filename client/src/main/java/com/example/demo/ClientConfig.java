ee26e57b427f9f188ccb3d2e3e75916689ef1209
Raja Kolli <rajadilipkolli@gmail.com>
2026-05-05 18:07:34 +0000

    revert and fix

--- client/src/main/java/com/example/demo/ClientConfig.java
+++ client/src/main/java/com/example/demo/ClientConfig.java
@@ -1,18 +1,13 @@
 package com.example.demo;
 
-
-import tools.jackson.databind.DeserializationFeature;
-import tools.jackson.databind.ObjectMapper;
-import tools.jackson.databind.SerializationFeature;
+import tools.jackson.databind.json.JsonMapper;
 import io.netty.channel.ChannelOption;
 import io.netty.handler.logging.LogLevel;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.MediaType;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
-import org.springframework.http.codec.json.Jackson2JsonDecoder;
-import org.springframework.http.codec.json.Jackson2JsonEncoder;
-import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.http.codec.json.JacksonJsonDecoder;
+import org.springframework.http.codec.json.JacksonJsonEncoder;
 import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.netty.http.client.HttpClient;
@@ -26,38 +21,42 @@ public class ClientConfig {
     }
 
     @Bean
-    public WebClient webClient(ObjectMapper objectMapper) {
+    public WebClient webClient(JsonMapper objectMapper) {
         var reactorHttpClient = HttpClient.create()
-                .wiretap("reactor.netty.http.client.HttpClient", LogLevel.DEBUG, AdvancedByteBufFormat.TEXTUAL)
+                .wiretap("reactor.netty.http.client.HttpClient", LogLevel.DEBUG,
+                        AdvancedByteBufFormat.TEXTUAL)
                 .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 5000);
-        ReactorClientHttpConnector reactorClientHttpConnector = new ReactorClientHttpConnector(reactorHttpClient);
+        ReactorClientHttpConnector reactorClientHttpConnector = new ReactorClientHttpConnector(
+                reactorHttpClient);
 
         return WebClient.builder()
-                //see: https://github.com/jetty-project/jetty-reactive-httpclient
-                //.clientConnector(new JettyClientHttpConnector())
+                // see: https://github.com/jetty-project/jetty-reactive-httpclient
+                // .clientConnector(new JettyClientHttpConnector())
                 .clientConnector(reactorClientHttpConnector)
                 .codecs(
                         clientCodecConfigurer -> {
                             // use defaultCodecs() to apply DefaultCodecs
                             // clientCodecConfigurer.defaultCodecs();
 
-                            // alter a registered encoder/decoder based on the default config.
+                            // alter a registered encoder/decoder based on the default
+                            // config.
                             // clientCodecConfigurer.defaultCodecs().jackson2Encoder(...)
 
                             // Or
                             // use customCodecs to register Codecs from scratch.
                             clientCodecConfigurer.customCodecs()
-                                    .register(new Jackson2JsonDecoder(objectMapper));
+                                    .register(new JacksonJsonDecoder(objectMapper));
                             clientCodecConfigurer.customCodecs()
-                                    .register(new Jackson2JsonEncoder(objectMapper));
+                                    .register(new JacksonJsonEncoder(objectMapper));
                         }
 
                 )
                 .exchangeStrategies(ExchangeStrategies.withDefaults())
-//                .exchangeFunction(ExchangeFunctions.create(new ReactorClientHttpConnector())
-//                        .filter(ExchangeFilterFunction.ofRequestProcessor(clientRequest -> {})))
-//                .filter(ExchangeFilterFunction.ofRequestProcessor(clientRequest -> {clientRequest.}))
-                //.defaultHeaders(httpHeaders -> httpHeaders.addAll())
+                // .exchangeFunction(ExchangeFunctions.create(new ReactorClientHttpConnector())
+                // .filter(ExchangeFilterFunction.ofRequestProcessor(clientRequest -> {})))
+                // .filter(ExchangeFilterFunction.ofRequestProcessor(clientRequest ->
+                // {clientRequest.}))
+                // .defaultHeaders(httpHeaders -> httpHeaders.addAll())
                 .baseUrl("http://localhost:8080")
                 .build();
     }
