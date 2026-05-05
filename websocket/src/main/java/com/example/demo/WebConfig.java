17a02835bbca5474271f3d5054e9d6956183ecf7
Raja Kolli <rajadilipkolli@gmail.com>
2026-05-05 17:43:12 +0000

    upgrade

--- websocket/src/main/java/com/example/demo/WebConfig.java
+++ websocket/src/main/java/com/example/demo/WebConfig.java
@@ -5,7 +5,7 @@
  */
 package com.example.demo;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import tools.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
