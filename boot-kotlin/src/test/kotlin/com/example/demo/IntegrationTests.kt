b22a04160154ef0d34603b8d1ad44632a06cfe8c
Raja Kolli <rajadilipkolli@gmail.com>
2026-05-16 06:28:04 +0000

    attempt to fix

--- boot-kotlin/src/test/kotlin/com/example/demo/IntegrationTests.kt
+++ boot-kotlin/src/test/kotlin/com/example/demo/IntegrationTests.kt
@@ -5,13 +5,14 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.boot.test.web.server.LocalServerPort
+import org.springframework.boot.webtestclient.autoconfigure.AutoConfigureWebTestClient
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.beans.factory.annotation.Autowired
 import reactor.test.StepVerifier
 
+@AutoConfigureWebTestClient
 @SpringBootTest(classes = [DemoApplication::class, TestcontainersConfiguration::class], webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class IntegrationTests {
 
