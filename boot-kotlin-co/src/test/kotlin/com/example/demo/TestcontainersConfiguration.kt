8d174f5288e0b58166ddff681bdc267f1185f1b4
Raja Kolli <rajadilipkolli@gmail.com>
2026-05-16 06:47:25 +0000

    upgrade all kotlin modules

--- boot-kotlin-co/src/test/kotlin/com/example/demo/TestcontainersConfiguration.kt
+++ boot-kotlin-co/src/test/kotlin/com/example/demo/TestcontainersConfiguration.kt
@@ -12,7 +12,7 @@ class TestcontainersConfiguration {
     @Bean
     @ServiceConnection
     fun mongoDbContainer(): MongoDBContainer {
-        return MongoDBContainer(DockerImageName.parse("mongo:latest"))
+        return MongoDBContainer(DockerImageName.parse("mongo").withTag("8.3.2")).withSharding()
     }
 
 }
